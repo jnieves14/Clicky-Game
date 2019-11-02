dc5af2cd01e3a09103ff0d4cb03729eca72b55fe
Jannah June Nieves <jannahjune@gmail.com>
2019-11-01 23:07:20 -0700

    paper component added

---
+++ src/Paper/Paper.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Paper from "@material-ui/core/Paper";
+
+const Paper = props => (
+    <Paper>
+        {props.children}
+    </Paper>
+)
+
+export default Paper;
