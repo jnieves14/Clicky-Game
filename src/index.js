35b35393adfdcffce1cfd30e6bf09dfe153a3485
Jannah June Nieves <jannahjune@gmail.com>
2019-11-02 20:25:51 -0700

    ninja naruto font added to src/fonts

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// import './fonts/Ninja-Naruto/njnaruto.woff'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
